4020238fcb61171dc6e3378cb1ffd661c4e922eb
mooky <mooky1007@naver.com>
2024-11-18 10:18:29 +0900

    update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,18 +2,26 @@ import React, { useEffect, useState } from 'react';
 
 import Hedaer from '../layouts/Hedaer';
 import Layout from '../layouts/Layout';
-import { Link } from 'react-router-dom';
 import TodoContainer from '../components/todo_container/TodoContainer';
 import RecordContainer from '../components/record_container/RecordContainer';
 
-// import logo from './logo.svg';
 const Home = ({ title }) => {
+    const [mode, setMode] = useState('todo');
+
     return (
         <Layout className="home">
             <Hedaer title={title} />
-            <div className="container">
+            <div className={`container ${mode}`}>
                 <TodoContainer />
                 <RecordContainer />
+                <div className="mobile_button_wrap">
+                    <button onClick={() => setMode('todo')} className={mode === 'todo' ? 'selected' : ''}>
+                        <img src="/assets/icons/todo.png" alt="" />
+                    </button>
+                    <button onClick={() => setMode('record')} className={mode === 'record' ? 'selected' : ''}>
+                        <img src="/assets/icons/record.png" alt="" />
+                    </button>
+                </div>
             </div>
         </Layout>
     );
